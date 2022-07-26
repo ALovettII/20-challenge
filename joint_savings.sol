1069e24e1bf41f8bfaabe648fc4e939f77fb66cb
Arthur Lovett <github@arthurlovett.com>
2022-07-25 17:13:12 -0700

    Complete: Step 1
    Completed Step 1: Create a Joint Savings Account Contract in Solidity. Added contractBalance setter to the withdraw function. Then added a setter for both accounts. Finally, added a default fallback function to store ether sent from outside the deposit function.

--- joint_savings.sol
+++ joint_savings.sol
@@ -70,7 +70,7 @@ contract JointSavings {
         /*
         Call the `contractBalance` variable and set it equal to the balance of the contract by using `address(this).balance`.
         */
-        contractBalance = address(this).balance
+        contractBalance = address(this).balance;
     }
 
     /*
@@ -79,11 +79,12 @@ contract JointSavings {
     function setAccounts(address payable account1, address payable account2) public{
 
         // Set the values of `accountOne` and `accountTwo` to `account1` and `account2` respectively.
-        // YOUR CODE HERE!
+        accountOne = account1;
+        accountTwo = account2;
     }
 
     /*
     Finally, add the **default fallback function** so that your contract can store Ether sent from outside the deposit function.
     */
-    // YOUR CODE HERE!
+    function() external payable {}
 }
